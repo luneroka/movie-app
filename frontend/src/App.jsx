cb46967ec84ab15605273708e7206afc213acb8d
Yoann Robert <luneroka@gmail.com>
2025-01-16 13:44:57 +0100

    Created Home and Favorites empty pages routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import MovieCard from './components/MovieCard';
 
 function App() {
   return (
     <>
-      
+      <MovieCard movie={{title: 'SpongeBob', release_date: '2025'}} />
     </>
   );
 }
