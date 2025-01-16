37aec6ac7eef04f7fe654c834e7e81a1ef0879fe
Yoann Robert <luneroka@gmail.com>
2025-01-16 14:05:28 +0100

    Added state to search input with onChange attribute and value

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from '../components/MovieCard';
+import { useState } from 'react';
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState('');
+
   const movies = [
     { id: 1, title: 'John Wick', release_date: '2014' },
     { id: 2, title: 'Batman Begins', release_date: '2005' },
@@ -18,6 +21,8 @@ function Home() {
           type='text'
           placeholder='Search for movies...'
           className='search-input'
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type='submit' className='search-button'>
           Search
